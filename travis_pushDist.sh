e8707cc6c476113da00f6a22b1cb3af8a5f6e4b1
pmunin(pmunin-ntb3) <philipp.munin@gmail.com>
2017-07-19 12:11:04 -0400

    Travis build pushing back only dist folder

--- travis_pushDist.sh
+++ travis_pushDist.sh
@@ -1,4 +1,4 @@
-git commit --message "[skip ci] Travis build: $TRAVIS_BUILD_NUMBER"
+git commit --message "[skip ci] Travis build: $TRAVIS_BUILD_NUMBER" -- dist
 git remote rm origin
 git remote add origin https://${GITHUB_TOKEN}@github.com/pmunin/handsontable.git
 git push --quiet --set-upstream origin master

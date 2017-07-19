351223ed657085ef788feb118b7ef982f8a1f830
pmunin(pmunin-ntb3) <philipp.munin@gmail.com>
2017-07-19 13:36:07 -0400

    [skip ci] removing redundant file for travis

--- travis_pushDist.sh
+++
@@ -1,4 +0,0 @@
-git commit --message "[skip ci] Travis build: $TRAVIS_BUILD_NUMBER" -- dist
-git remote rm origin
-git remote add origin https://${GITHUB_TOKEN}@github.com/pmunin/handsontable.git
-git push --quiet --set-upstream origin master

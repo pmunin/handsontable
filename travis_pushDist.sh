f2ae23d1abcc24345319413b4cb507a26630f6ef
pmunin(pmunin-ntb3) <philipp.munin@gmail.com>
2017-07-19 12:10:14 -0400

    Updating travis to push /dist files back to github

---
+++ travis_pushDist.sh
@@ -0,0 +1,4 @@
+git commit --message "[skip ci] Travis build: $TRAVIS_BUILD_NUMBER"
+git remote rm origin
+git remote add origin https://${GITHUB_TOKEN}@github.com/pmunin/handsontable.git
+git push --quiet --set-upstream origin master

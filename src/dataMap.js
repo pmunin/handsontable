592f11156abbf01e67458227fee9b1a0355aa0fb
work <work>
2017-07-27 11:33:53 -0400

    columns:{data:function(){}} - receives DataMap as this with access to handsontable and datasource

--- src/dataMap.js
+++ src/dataMap.js
@@ -552,7 +552,7 @@ DataMap.prototype.get = function(row, prop) {
      *      }
      *    }]}
      */
-    value = prop(this.dataSource.slice(row, row + 1)[0], undefined, row, this.dataSource);
+    value = prop.call(this, this.dataSource.slice(row, row + 1)[0], undefined, row);
   }
 
   if (this.instance.hasHook('modifyData')) {
@@ -632,7 +632,7 @@ DataMap.prototype.set = function(row, prop, value, source) {
 
   } else if (typeof prop === 'function') {
     /* see the `function` handler in `get` */
-    prop(this.dataSource.slice(row, row + 1)[0], value, row, this.dataSource);
+    prop.call(this, this.dataSource.slice(row, row + 1)[0], value, row);
 
   } else {
 
